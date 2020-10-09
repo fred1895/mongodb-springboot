45a853b34d1990b91d290aabd7f808eff74bc0fb
Fred Almeida <fredericonascimento97@gmail.com>
2020-10-09 16:58:34 -0300

    Criando instanciacao na base de dados

---
+++ src/main/java/io/github/fred1895/cursomongodb/config/Instantiation.java
@@ -0,0 +1,26 @@
+package io.github.fred1895.cursomongodb.config;
+
+import io.github.fred1895.cursomongodb.domain.User;
+import io.github.fred1895.cursomongodb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
--- src/main/java/io/github/fred1895/cursomongodb/domain/User.java
+++ src/main/java/io/github/fred1895/cursomongodb/domain/User.java
@@ -1,5 +1,6 @@
 package io.github.fred1895.cursomongodb.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -9,6 +10,7 @@ import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Document
 public class User implements Serializable {
     @Id
