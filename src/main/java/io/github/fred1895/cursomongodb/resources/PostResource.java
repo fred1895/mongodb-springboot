ae6f881cbe5d9e9564edb330f0d7aca71168a506
Fred Almeida <fredericonascimento97@gmail.com>
2020-10-15 11:37:21 -0300

    Adicionando POST Resource

---
+++ src/main/java/io/github/fred1895/cursomongodb/resources/PostResource.java
@@ -0,0 +1,26 @@
+package io.github.fred1895.cursomongodb.resources;
+
+import io.github.fred1895.cursomongodb.domain.Post;
+import io.github.fred1895.cursomongodb.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+
+    }
+
+}
---
+++ src/main/java/io/github/fred1895/cursomongodb/services/PostService.java
@@ -0,0 +1,31 @@
+package io.github.fred1895.cursomongodb.services;
+
+import io.github.fred1895.cursomongodb.domain.Post;
+import io.github.fred1895.cursomongodb.exception.ObjectNotFoundException;
+import io.github.fred1895.cursomongodb.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    public List<Post> findAll() {
+        return repository.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repository.deleteById(id);
+    }
+
+}
